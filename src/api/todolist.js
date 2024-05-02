d7c18d9a00fe22fbac4d19c627ccf02f51aa2d33
yeeun <84173008+jye104@users.noreply.github.com>
2024-05-02 16:47:39 +0900

    api -> Promise로 받아오게끔 수정

--- src/api/todolist.js
+++ src/api/todolist.js
@@ -3,5 +3,7 @@ import { useEffect } from 'react';
 // 데이터 불러오는곳~
 export const getToDoList = () => {
   const todo = localStorage.getItem('todos');
-  return todo ? JSON.parse(todo) : [];
+  return Promise.resolve({
+    data: todo ? JSON.parse(todo) : [],
+  });
 };
--- src/hooks/useToList.jsx
+++ src/hooks/useToList.jsx
@@ -3,7 +3,10 @@ import { getToDoList } from '../api/todolist';
 export const useTodoList = () => {
   return useQuery({
     queryKey: TodoKey.list,
-    queryFn: getToDoList,
+    queryFn: () => {
+      return getToDoList().then(({ data }) => data);
+      // Axios는 data로 감싸주기 때문에 함수를 하나 더 만들어서 내부에서 호출해주기!
+    },
   });
 };
 
