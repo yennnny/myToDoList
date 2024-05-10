4ed44ac547f029cb192e528fd85395c51497bc69
yeeun <84173008+jye104@users.noreply.github.com>
2024-05-10 15:46:05 +0900

    add: 자주쓰는 날짜 불러오는 함수를 utils에 만들기

---
+++ src/utils/getDate.js
@@ -0,0 +1,39 @@
+export const formatDateYYYYMMDD = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+};
+
+export const formatDateWeekdayDD = (date) => {
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  const dayOfWeek = daysOfWeek[date.getDay()];
+  const day = date.getDate();
+  const monthName = monthNames[date.getMonth()];
+
+  return `${dayOfWeek}, ${day} ${monthName}`;
+};
