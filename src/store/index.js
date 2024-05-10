d5415fd409da02186da60564536d19541a2dc1ca
yeeun <84173008+jye104@users.noreply.github.com>
2024-05-10 15:46:36 +0900

    add: todo를 추가하는 모달의 slice를 store에 추가하기

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import toggleSlice from './slices/modal';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    showModal: toggleSlice,
+  },
 });
 
 export default store;
